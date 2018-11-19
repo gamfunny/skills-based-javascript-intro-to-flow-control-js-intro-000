966d235838942632784cbf045fb3b1b7ccabeb3c
gam funnie <gamfunny@gmail.com>
2018-11-19 08:02:12 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -34,6 +34,9 @@ function ternaryTeenager(age) {
   if (age >=13 && age <=19) {
     return "You are a teenager"
   }
+  else {
+    return "You are not a teenager"
+  }
 }
 
 function switchAge(age) {
