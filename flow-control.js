72cc69ff642a78bed16bfeb69b109875970a996f
gam funnie <gamfunny@gmail.com>
2018-11-19 07:51:52 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,34 @@
 function basicTeenager(age) {
+ if(age >=13 && age <=19){
+   return "You are a teenager!"
+ }
 
 }
+basicTeenager(12);
+
 
 function teenager(age) {
-
+  if(age >=13 && age <=19){
+    return "You are a teenager!"
+  }
+  else {
+    return "You are not a teenager"
+  }
 }
+teenager(13);
+teenager(12);
+teenager(29);
 
 function ageChecker(age) {
-
+  if (age >=13 && age <=19) {
+    // condition is false hence code is not executed
+  } else if (age <= 12) {
+    // execute this code if `conditionToTest1` statement is falsey AND `conditionToTest2` is truthy
+  } else {
+    // execute this code if none of the other conditions are met
+  }
 }
+
 
 function ternaryTeenager(age) {
 
